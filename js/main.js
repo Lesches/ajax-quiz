5fc389f5b01f913bfbae2e541c6b89343303b767
Lesches <sholomls@gmail.com>
2019-03-06 12:22:49 -0500

    submitted

--- js/main.js
+++ js/main.js
@@ -1,26 +1,36 @@
 //Run jQuery after the document is fully loaded.
 $(document).ready(
     //The function that does the stuff.
+
+
     function () {
-        //Make the AJAX call
-        $.ajax('http://api.tvmaze.com/singlesearch/shows?q=the+magicians&embed=episodes', {
-            method: "GET",
-            dataType: "json"
-        })
-        //After the data comes back, use this function
-            .done(
+
+        $('#show').click(function(event){
+            event.preventDefault();
+
+            $.ajax('http://api.tvmaze.com/singlesearch/shows?q=the+magicians&embed=episodes', {
+                method: "GET",
+                dataType: "json"
+            }).done(
                 function (data) {
+
                     //Add the name
-                    $('#name').append(data.name);
+                    $(show).append(data.name);
+
+
                     //Add the episodes
                     data._embedded.episodes.forEach(function (episode) {
-                        $('#episodeList').append('<tr>'+
+                        $('#episodeList').append('<tr>' +
                             '<td>' + episode.season + '</td>' +
                             '<td>' + episode.number + '</td>' +
                             '<td>' + episode.name + '</td>' +
                             '<td>' + episode.summary + '</td>' +
                             +' </tr>')
-            })
+                    })
+                })
+
         })
-    }
+
+
+                }
 )
